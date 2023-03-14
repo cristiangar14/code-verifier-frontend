c6388a2ab77e7903360edafd5901faedb6b288e0
SoftPymesCgarzon <desarrollo.cristian.garzon@gmail.com>
2023-03-13 23:17:43 -0500

    adjusting the call to all katas

--- src/pages/KatasPage.tsx
+++ src/pages/KatasPage.tsx
@@ -1,16 +1,37 @@
-import React, { useEffect } from 'react';
+import { AxiosResponse } from 'axios';
+import React, { useEffect, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { useSessionStorage } from '../hooks/useSessionStorage';
+import { getAllKatas } from '../services/katasServices';
+import { Kata } from '../utils/types/Kata.type';
 
 const KatasPage = () => {
   
   let loggedIn = useSessionStorage('sessionJWTToken');
   let navigate = useNavigate();
-
+  // State of component
+  const [katas, setKatas] = useState([]) // initial katas is empty
+  const [totalPages, setTotalPages] = useState(1) // initial default value
+  const [currentPage, setCurrentPage] = useState(1) // initial default value
 
   useEffect(() => {
     if (!loggedIn) {
       return navigate('/login')
+    } else {
+      getAllKatas(loggedIn, 2, 1)
+        .then((response: AxiosResponse) => {
+          if (response.status === 200 && response.data.katas && response.data.currentPage && response.data.totalPages) {
+              console.table(response.data);
+              let { katas, totalPages, currentPage } = response.data;
+              setKatas(katas);
+              setTotalPages(totalPages);
+              setCurrentPage(currentPage);
+          } else  {
+            throw new Error(`[Error obtaining katas]:${response.data} `);
+            
+          }
+        })
+        .catch((err) => console.error(`[GET ALL KATAS ERROR]: ${err}`))
     }
   }, [loggedIn])
   
@@ -19,23 +40,36 @@ const KatasPage = () => {
    * method to navigate to kata details
    * @param {number} id of kata to navigate to
    */
-  const navigateToKataDetail = (id:number) => {
+  const navigateToKataDetail = (id:string) => {
     navigate(`/katas/${id}`)
   }
 
   return (
     <div>
       <h1>Katas page</h1>
-      {/* TODO: Real katas */}
-      <ul>
-        {/* TODO export to osilated component */}
-        <li onClick={() => navigateToKataDetail(1)}>
-            first kata
-        </li>
-        <li onClick={() => navigateToKataDetail(2)}>
-            Second kata
-        </li>
-      </ul>
+      {
+        katas.length > 0 ? 
+        <div className="">
+          
+            {/* TODO export to osilated component */}
+            {
+              katas.map((kata:Kata) => (
+                  <div key={kata._id}>
+                    <h3 onClick={() => navigateToKataDetail(kata._id)}>{kata.name}</h3>
+                    <h4>{kata.description}</h4>
+                    <h5>Creator: {kata.creator}</h5>
+                    <p>Rating: {kata.stars}/5</p>
+                  </div>
+                ))
+            }
+         
+        </div>
+        : 
+        <div className="">
+          <h5>No katas found</h5>
+        </div>
+      }
+      
     </div>
   )
 }
