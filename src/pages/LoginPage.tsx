05aea42aff8167fd52c36872ed3d1463afecf9b5
SoftPymesCgarzon <desarrollo.cristian.garzon@gmail.com>
2023-03-14 23:10:23 -0500

    Testing LoginMaterial component

--- src/pages/LoginPage.tsx
+++ src/pages/LoginPage.tsx
@@ -1,10 +1,12 @@
 import React from 'react'
-import LoginForm from '../components/forms/LoginForm'
+// import LoginForm from '../components/forms/LoginForm'
+import { LoginMaterial } from '../components/forms/LoginMaterial'
 
 const LoginPage = () => {
   return (
     <div>
-      <LoginForm />
+      {/* <LoginForm /> */}
+      <LoginMaterial />
     </div>
   )
 }
