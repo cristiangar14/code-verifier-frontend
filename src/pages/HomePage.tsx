4124bf72763af8838089f8c3034c607f6a09dffd
SoftPymesCgarzon <desarrollo.cristian.garzon@gmail.com>
2023-03-14 23:09:20 -0500

    testing Dashboard component

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,8 +1,11 @@
 import React from 'react'
+import { Dashboard } from '../components/dashboard/Dashboard'
 
 const HomePage = () => {
   return (
-    <div>HomePage home </div>
+    <div>
+      <Dashboard />  
+    </div>
   )
 }
 
